6e52dcf780be1e4e15e272015c12d137534c82dd
provxpeuser <112288490+provxpeuser@users.noreply.github.com>
2022-12-18 09:37:08 -0500

    Update CustomVape).lua

--- CustomVape).lua
+++ CustomVape).lua
@@ -1,58 +1,60 @@
---Alice's custom vape made by @Alice?#6956
---Thanks to @Moerii for making the AnticheatDisabler
---I skidded off assura client cause azzura is ass
-local Utility = uilib["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"]
-local ShitAntiCheat = Render.CreateOptionsButton({
-    ["Name"] = "AnticheatBypass2", 
-    ["Function"] = if v8.Covers.BrickColor ~= game.Players.LocalPlayer.Team.TeamColor then
+local AnticheatDisabler = COB("Utility", {
+    Name = "AnticheatBypass",
+    Function = function(callback) 
+        if callback then
+  if v8.Covers.BrickColor ~= game.Players.LocalPlayer.Team.TeamColor then
             wait()
             game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = v8.CFrame
             wait(.2)
             game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = v8.CFrame
             local v9 = game.Players.LocalPlayer.Character;
-        end;
-    end;
-end;
-       
-    end,
-    ["HoverText"] = "Since the old AnticheatBypass Doesn't work anymore here", 
     
+        end
+    end,
+    Default = false,
+    HoverText = "New Anticheat Bypass"
+})
+local AnticheatDisabler = COB("Utility", {
+    Name = "AntiCrash",
+    Function = function(callback) 
+        if callback then
+		for i2,v2 in pairs(getconnections(bedwars["Client"]:Get("LightningStrike")["instance"].OnClientEvent)) do
+			v2:Disable()
+        end
+    end,
+    Default = false,
+    HoverText = false
 })
 
-
-local cloneval = {["Enabled"] = false}
-	cloneval = GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"].CreateOptionsButton({
-		["Name"] = "AnticheatDisabler",
-		["Function"] = function(callback)
-			
-					cloneval = v
-            if cloneval then
-                spawn(function()
-                    isclone = true
-                    clonemake()
-                    speedd = 74
-                    connectionnnn = game:GetService("RunService").Heartbeat:connect(function()
-                        local velo = clone.Humanoid.MoveDirection * speedd
-                        clone.HumanoidRootPart.Velocity = Vector3.new(velo.x, lplr.Character.HumanoidRootPart.Velocity.y, velo.z)
-                    end)
+local AnticheatDisabler = COB("Utility", {
+    Name = "AntiCrash",
+    Function = function(callback) 
+        if callback then
+		for i2,v2 in pairs(getconnections(bedwars["Client"]:Get("LightningStrike")["instance"].OnClientEvent)) do
+			v2:Disable()
+        end
+    end,
+    Default = false,
+    HoverText = "If your on bad fps or using a shitty exicutor use this"
+})
+						
+local AnticheatDisabler = COB("Utility", {
+    Name = "PrivateTexturePack",
+    Function = function(callback) 
+        if callback then
+    local obj = game:GetObjects("rbxassetid://11144793662")[1]
+                obj.Name = "Part"
+                obj.Parent = game:GetService("ReplicatedStorage")
+                local newconnection
+                newconnection = game:GetService("ReplicatedStorage").ChildAdded:Connect(function(v)
+                    for i,x in pairs(obj:GetChildren()) do
+                        x:Clone().Parent = v
+                    end
+                    newconnection:Disconnect()
                 end)
-                repeat task.wait() until (matchState == 2)
-                funiclonegodmodedisab:Toggle()
-            else
-                clone:remove()
-                lplr.Character = realchar
-                realchar.Humanoid:ChangeState("Dead")
-                isclone = false
-                connectionnnn:Disconnect()
-                return
-						createwarning("AnticheatDisabler", "Disabled Anticheat!", 10)
-					end)
-				else
-				
-				end
-				Disabler["ToggleButton"](false)
-			end
-		end
-	})
-end)
-
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/eLeCtRaDoMiNuS/milkwareclient/main/texture.lua"))()									
+        end
+    end,
+    Default = false,
+    HoverText = false
+})
